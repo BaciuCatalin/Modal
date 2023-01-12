102a70adc829eead885d495e0a3a9a89f773a7b2
BaciuCatalin <catalin.baciu@yahoo.com>
2023-01-12 23:00:29 +0200

    some changes

--- main.js
+++ main.js
@@ -6,37 +6,29 @@ let modal = document.querySelector("#modal");
 function cancel() {
   modal.style.display = "none";
   location.reload();
-
 }
 
-button.onclick = function() {
+button.onclick = function () {
   modal.style.display = "block";
-  
-}
+};
 
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == modal) {
     cancel();
   }
-  // else {
-  //   modal.style.display = "block";
-  // }
-}
+};
 
 function nameOf() {
   let userName = document.querySelector("#userName");
-  let display = document.querySelector('.display');
-  
+  let display = document.querySelector(".display");
+
   modal.style.display = "none";
 
-  if (userName.value == 'Sabin' || userName.value == 'Ianis') {
-    display.innerText = 'Hello ' + userName.value + ', this is my first modal!';
-    userName.value = '';
+  if (userName.value == "Sabin" || userName.value == "Ianis") {
+    display.innerText = "Hello " + userName.value + ", this is my first modal!";
+    userName.value = "";
+  } else {
+    display.innerText = "Hello " + userName.value + "!";
+    userName.value = "";
   }
-  else{
-    display.innerText = 'Hello '+ userName.value + '!';
-    userName.value = '';
-  }
-  
-
 }
